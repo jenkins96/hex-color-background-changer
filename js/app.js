862f9c8519bb5406fa58b52c3f3c3cda6d5fc024
jenkins96 <adrianjnkns@gmail.com>
2021-02-20 14:31:11 +0000

    working on my own js

--- js/app.js
+++ js/app.js
@@ -1,19 +1,6 @@
-(function() {
-    const button = document.querySelector('#btn')
-    const body = document.querySelector('body')
-    const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
-    const value = document.querySelector('#hex-value')
-
-    button.addEventListener('click', changeHex)
-
-    function changeHex(){
-        let hex = '#'
-
-        for (let i = 0; i < 6; i++){
-            const index = Math.floor(Math.random()*hexValues.length)
-            hex += hexValues[index]
-        }
-        value.textContent = hex
-        body.style.backgroundColor = hex
-    }
-} )()
+// An array containing all the hex values possibilities
+const hex = [];
+// Generating a random integer number for selecting a hex value in the array
+const index = Math.floor(Math.random() * hex.length);
+// Defining where in the html document the hex value will be display
+const display = document.getElementById("hex-value");
