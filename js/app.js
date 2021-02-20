b6e2547af604e437515185b740d503363ff01faf
jenkins96 <adrianjnkns@gmail.com>
2021-02-20 15:37:32 +0000

    now working

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 // An array containing all the hex values possibilities
-const hex = ["#1B4F72", "#D6EAF8", "#CB4335", "#943126", "#C39BD3", "#F1948A", "#FFEB3B"];
-// Generating a random integer number for selecting a hex value in the array
-const index = Math.floor(Math.random() * hex.length);
+const hex = ["#1B4F72", "#D6EAF8", "#CB4335", "#943126", "#C39BD3", "#F1948A", "#FFEB3B", "#3c91e6", "#342E37", "#A2D729", "#FA824C"];
 // Defining where in the html document the hex value will be display
 const display = document.getElementById("hex-value");
 // Defining button
 const button = document.getElementById("btn");
 // Function
 changeColorOnClick = () => {
-    body.style.background = hex[index];
+// Generating a random integer number for selecting a hex value in the array
+    let index = Math.floor(Math.random() * hex.length);
+    document.body.style.background = hex[index];
     display.innerHTML = hex[index];
 }
 //Event Listener
