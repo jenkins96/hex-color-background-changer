e3dd4eeda683f74bb0c77291f629e458fc58f9ee
jenkins96 <59536240+jenkins96@users.noreply.github.com>
2021-02-21 10:43:47 -0600

    Updating to IIFE
    Using Immediately Invoked Function Expression

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/*
 // An array containing all the hex values possibilities
 const hex = ["#1B4F72", "#D6EAF8", "#CB4335", "#943126", "#C39BD3", "#F1948A", "#FFEB3B", "#3c91e6", "#342E37", "#A2D729", "#FA824C"];
 // Defining where in the html document the hex value will be display
@@ -13,3 +14,13 @@ changeColorOnClick = () => {
 }
 //Event Listener
 button.addEventListener("click", changeColorOnClick);
+*/
+// Using IIFE
+(function() {
+let hex = ["#1B4F72", "#D6EAF8", "#CB4335", "#943126", "#C39BD3", "#F1948A", "#FFEB3B", "#3c91e6", "#342E37", "#A2D729", "#FA824C"];
+let button = document.getElementById("btn").addEventListener("click", changeColorOnClick(){
+let index = Math.floor(Math.random() * hex.length);
+document.body.style.background = hex[index];
+document.getElementById("hex-value").innerHTML = hex[index];
+});
+})();
