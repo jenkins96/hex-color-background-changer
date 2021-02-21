f23ac272888292fcd5d323bfb910322f5b7c6dec
jenkins96 <adrianjnkns@gmail.com>
2021-02-21 16:50:11 +0000

    fixed minor bugs

--- js/app.js
+++ js/app.js
@@ -1,26 +1,13 @@
-/*
-// An array containing all the hex values possibilities
-const hex = ["#1B4F72", "#D6EAF8", "#CB4335", "#943126", "#C39BD3", "#F1948A", "#FFEB3B", "#3c91e6", "#342E37", "#A2D729", "#FA824C"];
-// Defining where in the html document the hex value will be display
-const display = document.getElementById("hex-value");
-// Defining button
-const button = document.getElementById("btn");
-// Function
-changeColorOnClick = () => {
-// Generating a random integer number for selecting a hex value in the array
-    let index = Math.floor(Math.random() * hex.length);
-    document.body.style.background = hex[index];
-    display.innerHTML = hex[index];
-}
-//Event Listener
-button.addEventListener("click", changeColorOnClick);
-*/
 // Using IIFE
 (function() {
+// Declaring array of colors
 let hex = ["#1B4F72", "#D6EAF8", "#CB4335", "#943126", "#C39BD3", "#F1948A", "#FFEB3B", "#3c91e6", "#342E37", "#A2D729", "#FA824C"];
-let button = document.getElementById("btn").addEventListener("click", changeColorOnClick(){
+// Event Listener
+document.getElementById("btn").addEventListener("click", function(){
 let index = Math.floor(Math.random() * hex.length);
+// Changing background color
 document.body.style.background = hex[index];
+// Where to display value?
 document.getElementById("hex-value").innerHTML = hex[index];
 });
 })();
